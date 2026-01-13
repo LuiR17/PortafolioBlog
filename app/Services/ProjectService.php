6b610cf22a81ff5858bc849f786c266da21f90bf
LuiR17 <luiseduardoromero2002@gmail.com>
2026-01-12 22:07:06 -0500

    try to fixed a bug v1

--- app/Services/ProjectService.php
+++ app/Services/ProjectService.php
@@ -3,42 +3,78 @@
 namespace App\Services;
 
 use App\Models\Project;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Storage;
+use Throwable;
 
 class ProjectService
 {
-    
     public function store(array $data): Project
     {
-        
-        if (isset($data['preview_image'])) {
-            $data['preview_image'] = $data['preview_image']
-                ->store('projects', 's3');
-        }
+        try {
+            if (
+                isset($data['preview_image']) &&
+                $data['preview_image'] instanceof \Illuminate\Http\UploadedFile &&
+                $data['preview_image']->isValid()
+            ) {
+                $data['preview_image'] = $data['preview_image']
+                    ->store('projects', 's3');
+            }
 
-        return Project::create($data);
+            return Project::create($data);
+        } catch (Throwable $e) {
+            Log::error('Error creating project', [
+                'error' => $e->getMessage(),
+            ]);
+
+            throw $e;
+        }
     }
 
     public function update(Project $project, array $data): Project
     {
-        if (isset($data['preview_image'])) {
-        if ($project->preview_image) {
-            Storage::disk('s3')->delete($project->preview_image);
+        try {
+            if (
+                isset($data['preview_image']) &&
+                $data['preview_image'] instanceof \Illuminate\Http\UploadedFile &&
+                $data['preview_image']->isValid()
+            ) {
+                if ($project->preview_image) {
+                    Storage::disk('s3')->delete($project->preview_image);
+                }
+
+                $data['preview_image'] = $data['preview_image']
+                    ->store('projects', 's3');
+            }
+
+            $project->update($data);
+
+            return $project;
+        } catch (Throwable $e) {
+            Log::error('Error updating project', [
+                'project_id' => $project->id,
+                'error' => $e->getMessage(),
+            ]);
+
+            throw $e;
         }
-
-            $data['preview_image'] = $data['preview_image']
-                ->store('projects', 's3');
-        }
-
-        $project->update($data);
-
-        return $project;
     }
 
     public function delete(Project $project): void
     {
-        if ($project->preview_image) {
-            Storage::disk('s3')->delete($project->preview_image);
+        try {
+            if ($project->preview_image) {
+                Storage::disk('s3')->delete($project->preview_image);
+            }
+
+            $project->delete();
+        } catch (Throwable $e) {
+            Log::error('Error deleting project', [
+                'project_id' => $project->id,
+                'error' => $e->getMessage(),
+            ]);
+
+            throw $e;
         }
-        $project->delete();
-    }}
+    }
+}
